c4dec55e5dd0912c7f1a5f950e41871772157550
fcjkm <9715791@gmail.com>
2018-01-30 23:25:40 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,10 +1,21 @@
 package ru.job4j;
 
-public class Calculate {
+/**
+ * Calculate.
+ *
+ * @author Konstantin Inozemcev (9715791@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+ 
+   public class Calculate {
 
-    public static void main(String[] args) {
-
-        System.out.println("Hello Word");
-    }
-
-}
+       
+        /**
+         * Конструктор вывода строки в консоль
+         *
+         * @param args - args.
+         */
+        public static void main(String[] args) {
+            System.out.println("Hello World");
+        }
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,7 @@
+/**
+* Package for calculate task.
+*
+* @author Konstantin Inozemcev (9715791@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
