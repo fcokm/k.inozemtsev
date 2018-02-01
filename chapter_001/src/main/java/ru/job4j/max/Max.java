a0722fc0d28e8b010bcc8b2a78cb262382532d22
fcjkm <9715791@gmail.com>
2018-02-01 22:48:08 +0300

    3.2. Максимум из двух чисел [#189]

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,31 @@
+package ru.job4j.max;   
+
+
+   /**
+     *  Class Max решение задачи 3.1 .
+     *
+     * @author Konstantin Inozemcev (9715791@gmail.com)
+     * @version $Id$
+     * @since 0.1
+     */
+	 
+	 
+	 
+	    public class Max {
+
+        /**
+         *  Метод возвращает максимальное число. 
+         *
+         * @param first первое число.
+		 * @param second второе число.
+         * @return максимальное число.
+         */
+            public int max(int first, int second) {
+
+            return first > second ? first : second;
+        }
+    }
+	
+	
+	
+	
