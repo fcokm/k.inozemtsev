5c977653fc8146fd8cba7701dc9f6e50d003061c
fcokm <9715791@gmail.com>
2018-06-07 15:45:03 +0300

    Пример иллюстрирующий проблемы многопоточности

---
+++ chapter_004/src/main/java/ru/job4j/thread/CachePoint.java
@@ -0,0 +1,48 @@
+package ru.job4j.thread;
+/**
+ * Class CachePoint
+ *
+ * @author Konstantin Inozemcev (9715791@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class CachePoint {
+    static int moneyAmount = 100;
+
+    static  void getMoney(int money) {
+        if(moneyAmount >= money) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            moneyAmount -= money;
+            System.err.println("New amount: " + moneyAmount);
+        } else {
+            System.out.println("No enough money");
+        }
+    }
+
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+               getMoney(50);
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                getMoney(100);
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                getMoney(100);
+            }
+        }).start();
+    }
+}
