7fa0bed2ae2ad9f975ad23736a97e0778174a1c9
k.inozemcev <9715791@gmail.com>
2019-06-12 22:38:31 +0300

    Создать TODO list #3785

---
+++ chapter_007/src/main/java/ru/job4j/service/JsonServlet.java
@@ -0,0 +1,37 @@
+package ru.job4j.service;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import ru.job4j.model.Item;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Timestamp;
+
+
+public class JsonServlet extends HttpServlet {
+
+    DBStorage storage = DBStorage.getInstance();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/json");
+        PrintWriter writer = new PrintWriter(resp.getOutputStream());
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(writer, storage.allItem());
+        writer.flush();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        BufferedReader reader = req.getReader();
+        Item item = mapper.readValue(reader, Item.class);
+        item.setCreated(new Timestamp(System.currentTimeMillis()));
+        storage.addOrUpdate(item);
+        reader.close();
+    }
+
+}
